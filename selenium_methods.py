7f44f90e6419f5b340a4cc2ba8ba3ef665fe1711
alderven <alderven@gmail.com>
2017-12-30 02:23:00 +0300

    Add screenshot capture

--- selenium_methods.py
+++ selenium_methods.py
@@ -1,3 +1,5 @@
+import allure
+from allure_commons.types import AttachmentType
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -5,13 +7,19 @@ from selenium.webdriver.support import expected_conditions as EC
 TIMEOUT = 30
 
 
+def main(driver, ec, locator_type, locator):
+    element = WebDriverWait(driver, TIMEOUT).until(ec((locator_type, locator)))
+    allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+    return element
+
+
 def find_element_by_xpath(driver, xpath):
-    element = WebDriverWait(driver, TIMEOUT).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    element = main(driver, EC.presence_of_element_located, By.XPATH, xpath)
     return element
 
 
 def find_elements_by_xpath(driver, xpath):
-    elements = WebDriverWait(driver, TIMEOUT).until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
+    elements = main(driver, EC.presence_of_all_elements_located, By.XPATH, xpath)
     return elements
 
 
